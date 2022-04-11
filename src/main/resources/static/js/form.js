9048e1f2fc4e40461f96df66814fc50997fd2275
Dmytro Kriuchkov <kriuchkovdima@gmail.com>
2022-04-11 13:13:53 +0300

    Redesigned form inputs behaviour.

--- src/main/resources/static/js/form.js
+++ src/main/resources/static/js/form.js
@@ -1,7 +1,37 @@
+//As default - all inputs is valid.
+document.querySelectorAll("input, select").forEach((x) => x.classList.add("is-valid"));
+
+//Some browsers don't support number type
+document.getElementsByTagName("input[type=number]").value = 0;
+
+//Get today's date
+const currentDate = new Date();
+const day = ("0" + currentDate.getDate()).slice(-2);
+const month = ("0" + (currentDate.getMonth() + 1)).slice(-2);
+const today = currentDate.getFullYear() + "-" + (month) + "-" + (day);
+
+//Set default and standard value to today's date
+var dates = document.querySelectorAll("input[type=date]");
+for (let i = 0; i < dates.length; i++) {
+    dates[i].setAttribute("defaultValue", today);
+
+    if (!dates[i].getAttribute("value")) {
+        dates[i].setAttribute("value", today);
+    }
+}
+
+//Get list of inputs that may be invalid
 const validTypes = ["date"];
 const inputs = [...document.querySelectorAll("input")].filter((x) => !validTypes.includes(x.type));
 
 inputs.forEach((input) => {
+    //If input value is empty, make input invalid
+    const value = input.getAttribute("value");
+    if (!value || value === "") {
+        input.classList.remove("is-valid");
+    }
+
+    //Set event listeners
     input.addEventListener("blur", (event) => {
         if (event.target.value) {
             input.classList.add("is-valid");
@@ -10,16 +40,3 @@ inputs.forEach((input) => {
         }
     });
 });
-
-//Set "is-valid" css class for form elements
-document.querySelectorAll("input[type=date], select").forEach((x) => x.classList.add("is-valid"));
-
-//Set today's date as default for date input
-const currentDate = new Date();
-const day = ("0" + currentDate.getDate()).slice(-2);
-const month = ("0" + (currentDate.getMonth() + 1)).slice(-2);
-const today = currentDate.getFullYear() + "-" + (month) + "-" + (day);
-document.getElementById("date").value = today;
-
-//Some browsers don't support number type
-document.getElementsByTagName("input[type=number]").value = 0;
